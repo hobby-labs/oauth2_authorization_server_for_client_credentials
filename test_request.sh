502bb0abaa64a7fa38d5c14a56dfd441646b0af0
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-07-20 13:10:47 +0900

    Updated

--- test_request.sh
+++ test_request.sh
@@ -2,11 +2,21 @@
 
 main() {
     local response_body jwt jwt_header jwt_payload
-    response_body="$(curl -u my-client:my-secret -d "grant_type=client_credentials&scope=read" http://localhost:9000/oauth2/token)"
+    response_body="$(curl -u my-client:my-secret -d "grant_type=client_credentials&scope=read" http://localhost:9000/oauth2/token 2> /dev/null)"
+
+    if [ "${response_body}" == "" ]; then
+        echo "Error: No response received from the server http://localhost:9000/oauth2/token."
+        return 1
+    fi
+
     jwt=$(jq -r '.access_token' < <(curl -u my-client:my-secret -d "grant_type=client_credentials&scope=read" http://localhost:9000/oauth2/token))
-    jwt_header=$(echo -n ${jwt} | cut -d '.' -f 1 | base64 --decode)
-    jwt_payload=$(echo -n ${jwt} | cut -d '.' -f 2 | base64 --decode)
-    jwt_signature=$(echo -n ${jwt} | cut -d '.' -f 3)
+
+    IFS='.' read -r jwt_header jwt_payload jwt_signature <<< "$jwt"
+    jwt_header=$(echo -n "${jwt_header}" | base64 --decode)
+    jwt_payload=$(echo -n "${jwt_payload}" | base64 --decode)
+    #jwt_header=$(echo -n ${jwt} | cut -d '.' -f 1 | base64 --decode)
+    #jwt_payload=$(echo -n ${jwt} | cut -d '.' -f 2 | base64 --decode)
+    #jwt_signature=$(echo -n ${jwt} | cut -d '.' -f 3)
     echo "# Result of Raw Response ###################################################"
     echo "${response_body}" | jq -r '.'
 
