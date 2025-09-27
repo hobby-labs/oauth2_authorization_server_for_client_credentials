6bfaba1888bf7c8af06256fec24c108a17420a5b
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-09-28 02:58:43 +0900

    Renamed main class

--- src/main/java/com/github/TsutomuNakamura/oauth2_authorization_server_for_client_credentials/OAuth2ClientCredentialsAuthServer.java
+++ src/main/java/com/github/TsutomuNakamura/oauth2_authorization_server_for_client_credentials/OAuth2ClientCredentialsAuthServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyprojectApplication {
+public class OAuth2ClientCredentialsAuthServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyprojectApplication.class, args);
+		SpringApplication.run(OAuth2ClientCredentialsAuthServer.class, args);
 	}
 
 }
